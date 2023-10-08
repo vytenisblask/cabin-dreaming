e236bccfb2a5811850e6abef68d11b6cacfcb99e
vytenisblask <vytenisb@yahoo.com>
2023-10-08 21:09:42 +0300

    title and sliders bug

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Cabin Dreaming';
+      return args;
+    });
+  }
+}
